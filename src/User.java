ece3e1774edc2e58f940cceb49c31ab680c97fea
Alex_Barbu <p3200284@aueb.gr>
2024-04-04 00:07:24 +0300

    User

---
+++ src/User.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class user {
+    
+    public String UID;
+    
+    public int Name;
+    public int Surname;
+    public boolean isManager;
+
+    private String _managerID = "-1";
+    private Map<String, Reservation> _reservations = new HashMap<>();
+
+    public String managerID(){ return _managerID; } //Getter
+
+    public void setManagerID(String ID){ _managerID = ID;} //Setter - ONE TIME USE ONLY 
+
+    //------------------------MANAGER METHODS------------------------------------------
+
+
+    //------------------------CUSTOMEER METHODS----------------------------------------
+}
