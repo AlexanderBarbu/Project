faab1cf6efe00454e63989876b422a97afd5d677
Alex_Barbu <p3200284@aueb.gr>
2024-04-13 21:27:53 +0300

    manager, user methods

--- src/Model/User.java
+++ src/Model/User.java
@@ -1,6 +1,9 @@
 package Model;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class User {
@@ -14,6 +17,7 @@ public class User {
     private String _managerID = "-1";
     private Map<String, Reservation> _reservations = new HashMap<>();
     private Map<String, Hotel> _hotels = new HashMap<>();
+    private int reservations = 0;
 
     public String managerID(){ return _managerID; } //Getter
 
@@ -21,10 +25,36 @@ public class User {
 
     //------------------------MANAGER METHODS------------------------------------------
 
+    public void registerHotel(Hotel h){
+        h.setMID(_managerID);
+        _hotels.put(h.Name,h);
+    }
 
-    //------------------------CUSTOMEER METHODS----------------------------------------
+    public Map<String, Reservation> getReservations(Hotel h){
+        Map<String, Reservation> hotelReservations = new HashMap<>();
+        Map<String, Room> rooms = h.getRooms();
 
-    public void makeReservation(){
-        
+        for (Room room : rooms.values()){
+            List<Reservation> reservations = room.getreservations();
+
+            for(Reservation res : reservations){
+                hotelReservations.put(room.Name, res);
+            }
+        }
+
+        return  hotelReservations;
+    }
+
+    //------------------------CUSTOMER METHODS----------------------------------------
+
+    public boolean makeReservation(Hotel h, LocalDateTime start, LocalDateTime end, int people){
+        Room room = h.getAvailableRoom( people, start, end );
+        boolean availability = room != null;
+
+        if(availability){
+            String RID = reservations+"R"+UID;
+            room.addReservation(new Reservation(RID, start, end));
+        }
+        return availability;
     }
 }
